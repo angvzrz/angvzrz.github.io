09a01015d08741362ebbd192706c7ba1096d70bb
Angel Osiel <ang_vzrz@live.com.mx>
2020-09-05 16:09:27 -0700

    Components and Navigation structure  folders created

---
+++ src/components/Navigation/TopBar/TopBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TopBar = () =>
+    <header>
+        <p>Home</p>
+        <p>Skills</p>
+        <p>Work Experience</p>
+        <p>About me</p>
+        <p>Contact</p>
+    </header>
