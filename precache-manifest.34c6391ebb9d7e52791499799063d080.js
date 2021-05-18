ef82a7c8c6d51522ee17c0ecf390666520d0c5c4
Angel Osiel <ang_vzrz@live.com.mx>
2021-05-17 23:03:37 -0700

    Updates

---
+++ precache-manifest.34c6391ebb9d7e52791499799063d080.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7614c5a955a5c846b8b52e8b21c6933",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15b2c4667d3957a7b4d2",
+    "url": "/static/css/main.12ced01c.chunk.css"
+  },
+  {
+    "revision": "0a55df6fa487b5020514",
+    "url": "/static/js/2.b7422a22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b7422a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b2c4667d3957a7b4d2",
+    "url": "/static/js/main.0bec3435.chunk.js"
+  },
+  {
+    "revision": "a4a011ee4cff22dbe791",
+    "url": "/static/js/runtime-main.d32ca624.js"
+  },
+  {
+    "revision": "a2ad8b1f77e74f001b1ec065cd32d5b8",
+    "url": "/static/media/csslogo.a2ad8b1f.png"
+  },
+  {
+    "revision": "a5094f30e5dcec807d59b60230667c34",
+    "url": "/static/media/htmllogo.a5094f30.png"
+  },
+  {
+    "revision": "39a0289f3edf0c5b8f3d15d56f881ca4",
+    "url": "/static/media/mw_experience.39a0289f.svg"
+  },
+  {
+    "revision": "29c215a539f78fe399a119cc3ca82f34",
+    "url": "/static/media/mw_js_skill.29c215a5.svg"
+  },
+  {
+    "revision": "a90f6e99bed308fe78fef941427391c8",
+    "url": "/static/media/mw_python_skill.a90f6e99.svg"
+  },
+  {
+    "revision": "882b32781fed310ba3d1df33ec0252a3",
+    "url": "/static/media/mw_react_skill.882b3278.svg"
+  },
+  {
+    "revision": "42cf86bb1410b9c77cbe171eb871e708",
+    "url": "/static/media/profile_pic.42cf86bb.jpg"
+  }
+]);
